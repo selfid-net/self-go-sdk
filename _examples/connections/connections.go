7a99068eb59e5a48efc8c0c31e6f6bd31041be4a
Adrià Cidre <593270+adriacidre@users.noreply.github.com>
2020-10-13 09:57:38 +0200

    Connections example (#44)

--- _examples/connections/connections.go
+++ _examples/connections/connections.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	selfsdk "github.com/joinself/self-go-sdk"
+	"github.com/joinself/self-go-sdk/messaging"
 )
 
 // expects 1 argument - the Self ID you want to permit
@@ -28,30 +30,63 @@ func main() {
 		panic("you must specify a self id as an argument")
 	}
 
+	fmt.Println("CONNECTIONS EXAMPLE")
 	ms := client.MessagingService()
 
-	log.Println("revoking connection")
-	// revoke a connection
-	err = ms.RevokeConnection(os.Args[1])
+	err = listConnections(ms)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Block all connections")
+	err = ms.RevokeConnection("*")
 	if err != nil {
 		log.Fatal("revoke connection returned with: ", err)
 	}
+	err = listConnections(ms)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	log.Println("permitting connection")
-	// permit a connection
+	log.Println("Permit connections from a specific ID")
 	err = ms.PermitConnection(os.Args[1])
 	if err != nil {
 		log.Fatal("permitting connection returned with: ", err)
 	}
-
-	log.Println("listing connections")
-	// list all connections
-	connections, err := ms.ListConnections()
+	err = listConnections(ms)
 	if err != nil {
-		log.Fatal("listing connections returned with: ", err)
+		log.Fatal(err)
 	}
 
-	log.Println("connected to:", connections)
+	log.Println("Permit all connections (replaces all other entries with a wildcard entry)")
+	err = ms.PermitConnection("*")
+	if err != nil {
+		log.Fatal("permitting connection returned with: ", err)
+	}
+	err = listConnections(ms)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Permit connection from a specific ID (no change as the list already contains a wildcard entry)")
+	err = ms.PermitConnection(os.Args[1])
+	if err != nil {
+		log.Fatal("permitting connection returned with: ", err)
+	}
+	err = listConnections(ms)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	log.Println("acl commands succeeded")
+}
+
+func listConnections(ms *messaging.Service) error {
+	log.Println("List existing connections")
+	connections, err := ms.ListConnections()
+	if err != nil {
+		return err
+	}
+	log.Println(" - connections : ", connections)
+	return nil
 }
